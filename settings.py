39a4d076f9a6f6e8e13ed547b1f9cd6a6525ebf7
deveshmaan7 <deveshmaan7@gmail.com>
2025-09-05 16:21:14 +0530

    fetch nifty 50 and filter strike by 3 points

--- settings.py
+++ settings.py
@@ -2,3 +2,5 @@ instrument_list = ['OPTIDX'] # this will be index option instrument type
 futureOption_list = ['NIFTY'] # this will be index name NIFTY, BANKNIFTY
 option_list = ['CE', 'PE']
 expiry_list = ['09SEP2025']
+nifty50_token = ['99926000']
+exchange = 'NSE'
