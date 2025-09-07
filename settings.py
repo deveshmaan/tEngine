a72677ea768ab34d7b8a76a9f623c6abe14d86d2
deveshmaan7 <deveshmaan7@gmail.com>
2025-09-07 21:15:05 +0530

    added upstox sdk

--- settings.py
+++ settings.py
@@ -1,6 +1,13 @@
-instrument_list = ['OPTIDX'] # this will be index option instrument type
 futureOption_list = ['NIFTY'] # this will be index name NIFTY, BANKNIFTY
 option_list = ['CE', 'PE']
 expiry_list = ['09SEP2025']
 nifty50_token = ['99926000']
 exchange = 'NSE'
+instrument_list = ['OPTIDX'] # this will be index option instrument type
+
+
+instrument_list_upstox = ['INDEX']
+expiry_list_upstox = ['2025-09-09'] # format: YYYY-MM-DD
+segment = ['NSE_FO'] # NSE_FO for Futures and Options
+
+
